513731389a1339b09b4b8f7f48ef7d68f7e904da
Clayton Ray <iamclaytonray@gmail.com>
2017-10-25 14:10:10 -0400

    Delete redux/ in favor of the npm pkg

--- src/redux/actions/announcements.js
+++
@@ -1,9 +0,0 @@
-import * as types from './types';
-import { getData } from './index';
-
-// fetch announcements
-export function fetchAnnouncements() {
-  const url = `/announcements`;
-
-  return dispatch => getData(types.FETCH_ANNOUNCEMENTS_SUCCESS, types.FETCH_ANNOUNCEMENTS_FAILURE, url, dispatch);
-}
