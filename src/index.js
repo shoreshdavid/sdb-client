33690e6eeb29c3f72e23efb1eea1546ec6f5671a
Clayton Ray <iamclaytonray@gmail.com>
2017-11-06 02:40:37 -0500

    Update sdb-redux & remove store config

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import reduxThunk from 'redux-thunk';
 
 import { App } from 'components/App';
 import { Error } from 'containers/Error';
-import { rootReducer } from 'sdb-redux';
+import { store } from 'sdb-redux';
 import registerServiceWorker from './registerServiceWorker';
-
-// create store with middleware
-const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
-
-// redux store setup
-const store = createStoreWithMiddleware(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
 
 render(
   <Provider store={store}>
