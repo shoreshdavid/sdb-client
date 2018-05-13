edfc54369c927bb6106e6f0f70bdfe124deaa46b
Clayton Ray <iamclaytonray@gmail.com>
2018-05-13 04:40:08 -0400

    refactor: delete redux/store

--- src/redux/store/store.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-  export { configureStore } from './configureStore.prod';
-} else {
-  export { configureStore } from './configureStore.dev';
-}
--- src/redux/store/store.prod.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { rootReducer } from '../reducers';
-
-export const configureStore = preloadedState => createStore(
-  rootReducer,
-  preloadedState,
-  applyMiddleware(thunk)
-)
