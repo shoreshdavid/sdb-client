83efb7ead9b064a246551cbe0640bbc307d98ab6
Clayton Ray <iamclaytonray@gmail.com>
2018-06-25 10:43:07 -0400

    style/layout: complete Accordion component/styles

---
+++ src/components/Accordion/index.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { Button, Collapse } from 'reactstrap';
+
+export class Accordion extends React.Component<any, any> {
+  public state = { collapse: false };
+
+  public toggle = () => {
+    this.setState({ collapse: !this.state.collapse });
+  }
+
+  public render() {
+    const { text, heading } = this.props;
+    return (
+      <div>
+        <Button
+          color="primary"
+          block
+          onClick={this.toggle}
+          className="accordion-button"
+        >
+          <i className={`icon-primary fas fa-${this.state.collapse ? 'minus' : 'plus'}`} />{' '}
+          <span>{heading}</span>
+        </Button>
+        <Collapse isOpen={this.state.collapse}>
+          <div className="accordion-item-container">
+            <p>{text}</p>
+          </div>
+        </Collapse>
+      </div>
+    );
+  }
+}
