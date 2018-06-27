67788a2022b82dc53f92d37e7595c89018fc80fd
Clayton Ray <iamclaytonray@gmail.com>
2018-06-27 12:55:37 -0400

    refactor: move Root/ to src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,36 @@ import ApolloClient from 'apollo-boost';
 import * as React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { ErrorBoundary } from 'containers/ErrorBoundary';
-import { Root } from 'containers/Root';
-// import registerServiceWorker from './registerServiceWorker';
-import 'styles/application.css';
+
+// components
+import { Footer } from 'components/Footer';
+import { Header } from 'components/Header';
+import { SubFooter } from 'components/SubFooter';
+import { TopBar } from 'components/TopBar';
+
+// pages
+import { ArticleListPage } from 'pages/ArticleListPage';
+import { DonatePage } from 'pages/DonatePage';
+import { EventsPage } from 'pages/EventsPage';
+import { HomePage } from 'pages/HomePage';
+import { JewishPage } from 'pages/JewishPage';
+import { LiveStreamPage } from 'pages/LiveStreamPage';
+import { NotFoundPage } from 'pages/NotFoundPage';
+import { ProductListPage } from 'pages/ProductListPage';
+import { ServiceListPage } from 'pages/ServiceListPage';
+import { SingleArticle } from 'pages/SingleArticle';
+import { SingleServicePage } from 'pages/SingleServicePage';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ScrollToTop } from 'components/ScrollToTop';
+import 'style/scss/application.scss';
+import { API_URL } from './constants';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
+  uri: API_URL,
   request: async operation => {
     const token = await localStorage.getItem('token');
     operation.setContext({
@@ -24,12 +45,35 @@ const client = new ApolloClient({
 render(
   <ApolloProvider client={client}>
     <BrowserRouter>
-      <ErrorBoundary>
-        <Root />
-      </ErrorBoundary>
+      <ScrollToTop>
+        <ErrorBoundary>
+          <React.Fragment>
+            <TopBar />
+            <Header />
+
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route exact path="/live" component={LiveStreamPage} />
+              <Route exact path="/articles" component={ArticleListPage} />
+              <Route exact path="/articles/:slug" component={SingleArticle} />
+              <Route exact path="/store" component={ProductListPage} />
+              <Route exact path="/services" component={ServiceListPage} />
+              <Route
+                exact
+                path="/services/:slug"
+                component={SingleServicePage}
+              />
+              <Route exact path="/events" component={EventsPage} />
+              <Route exact path="/jewish" component={JewishPage} />
+              <Route exact path="/donate" component={DonatePage} />
+              <Route component={NotFoundPage} />
+            </Switch>
+            <Footer />
+            <SubFooter />
+          </React.Fragment>
+        </ErrorBoundary>
+      </ScrollToTop>
     </BrowserRouter>
   </ApolloProvider>,
   document.getElementById('root'),
 );
-
-// registerServiceWorker();
