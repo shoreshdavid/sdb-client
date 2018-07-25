1c7d2bf8635d7d25d584601814b2fb409c47d92f
Clayton Ray <iamclaytonray@gmail.com>
2018-07-25 00:42:31 -0400

    style: update Calendar styles

--- src/pages/EventsPage/data.ts
+++ src/pages/EventsPage/data.ts
@@ -1,8 +1,28 @@
-export const eventData = [
+let today = new Date();
+let y = today.getFullYear();
+let m = today.getMonth();
+let d = today.getDate();
+
+export const events = [
   {
-    id: 1,
-    name: `Men's Meeting & Free Zaxby's`,
-    date: 'July 24',
-    time: '7:30PM',
+    title: 'All Day Event',
+    allDay: false,
+    start: new Date(y, m, 1),
+    end: new Date(y, m, 1),
+    color: 'default',
+  },
+  {
+    title: 'Meeting',
+    start: new Date(y, m, d - 1, 10, 30),
+    end: new Date(y, m, d - 1, 11, 30),
+    allDay: false,
+    color: 'green',
+  },
+  {
+    title: 'Lunch',
+    start: new Date(y, m, d + 7, 12, 0),
+    end: new Date(y, m, d + 7, 14, 0),
+    allDay: false,
+    color: 'red',
   },
 ];
--- src/pages/EventsPage/index.tsx
+++ src/pages/EventsPage/index.tsx
@@ -1,19 +1,52 @@
+import * as moment from 'moment';
 import * as React from 'react';
-import Calendar from 'react-calendar';
+import BigCalendar from 'react-big-calendar';
 import { Col, Container, Row } from 'reactstrap';
+import { events } from './data';
+import './styles.scss';
 
-export class EventsPage extends React.Component<any, any> {
+BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment));
+
+export class EventsPage extends React.Component {
   public state = {
-    date: new Date(),
+    events: events,
+    alert: null,
   };
+  public selectedEvent = event => {
+    alert(event.title);
+  }
+  public eventColors = (event: any, start: any, end: any, isSelected: any) => {
+    let backgroundColor = 'event-';
+    event.color
+      ? (backgroundColor = backgroundColor + event.color)
+      : (backgroundColor = backgroundColor + 'default');
+    return {
+      className: backgroundColor,
+    };
+  }
   public render() {
     return (
-      <Container fluid className="events-page">
-        <Row>
-          <Col lg={{ size: 10, offset: 1 }}>
-            <Calendar value={this.state.date} />
-          </Col>
-        </Row>
+      <Container fluid className="padding-50">
+          <Row>
+            <Col
+              xs={12}
+              md={10}
+              xl={{ size: 6, offset: 3 }}
+              className="ml-auto mr-auto"
+            >
+              <BigCalendar
+                style={{ height: '100vh' }}
+                selectable
+                events={this.state.events}
+                defaultView="month"
+                scrollToTime={new Date(1970, 1, 1, 6)}
+                defaultDate={new Date()}
+                // onSelectEvent={event => this.selectedEvent(event)}
+                // onSelectSlot={slotInfo => this.addNewEventAlert(slotInfo)}
+                eventPropGetter={this.eventColors}
+              />
+            </Col>
+          </Row>
       </Container>
     );
   }
