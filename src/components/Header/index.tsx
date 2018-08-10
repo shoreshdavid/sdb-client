5ab12dd774bc9f8bba6a843c46b70d0a21cebad2
Clayton Ray <iamclaytonray@gmail.com>
2018-08-10 18:33:28 -0400

    improv: implement UI for header component (including mobile)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,35 +1,95 @@
+import * as moment from 'moment';
 import * as React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import {
+  Badge,
+  Collapse,
+  Nav,
+  Navbar,
+  NavbarToggler,
+  NavItem,
+} from 'reactstrap';
 
-export const Header = () => {
-  return (
-    <header className="clearfix">
-      <div className="header-links">
-        <NavLink exact to="/" activeClassName="navlink-active">
-          Home
-        </NavLink>
-        <NavLink exact to="/services" activeClassName="navlink-active">
-          Teachings
-        </NavLink>
-        <NavLink exact to="/live" activeClassName="navlink-active">
-          Streaming
-        </NavLink>
-        <NavLink exact to="/jewish" activeClassName="navlink-active">
-          Jewish
-        </NavLink>
-        <NavLink exact to="/discoveries" activeClassName="navlink-active">
-          Discoveries
-        </NavLink>
-        <NavLink exact to="/events" activeClassName="navlink-active">
-          Events
-        </NavLink>
-        <NavLink exact to="/store" activeClassName="navlink-active">
-          Store
-        </NavLink>
-        <NavLink exact to="/donate" activeClassName="navlink-active">
-          Donate
-        </NavLink>
+export class Header extends React.Component {
+  public state = {
+    isOpen: false,
+  };
+
+  public toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  }
+
+  public render() {
+    const today = moment();
+    const otherDate = moment().isoWeekday(5);
+    const isToday = today.toString() === otherDate.toString();
+
+    const renderStreamBadge = isToday ? (
+      <Badge style={{ backgroundColor: '#aa4ee0' }}>LIVE NOW!</Badge>
+    ) : null;
+
+    return (
+      <div className="sdb-nav">
+        <Navbar light expand="md">
+          <Link to="/">
+            <img
+              src="https://tikvaproductions.com/wp-content/uploads/2018/02/page-1-tree.png"
+              alt="Shoresh David Brandon logo"
+            />
+          </Link>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink exact to="/" activeClassName="navlink-active">
+                  Home
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink exact to="/services" activeClassName="navlink-active">
+                  Teachings
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink exact to="/live" activeClassName="navlink-active">
+                  Streaming {renderStreamBadge}
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink exact to="/jewish" activeClassName="navlink-active">
+                  Jewish
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  exact
+                  to="/discoveries"
+                  activeClassName="navlink-active"
+                >
+                  Discoveries
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink exact to="/events" activeClassName="navlink-active">
+                  Events
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink exact to="/store" activeClassName="navlink-active">
+                  Store
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink exact to="/donate" activeClassName="navlink-active">
+                  Donate <Badge color="danger">PLEASE</Badge>
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
       </div>
-    </header>
-  );
-};
+    );
+  }
+}
