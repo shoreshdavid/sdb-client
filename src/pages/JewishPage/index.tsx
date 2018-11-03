58e45e6ef29e0bd7bb2b0b70f1d9d7be8e817e00
Clayton Ray <iamclaytonray@gmail.com>
2018-11-02 21:48:03 -0400

    chore: deps/updates

--- src/pages/JewishPage/index.tsx
+++ src/pages/JewishPage/index.tsx
@@ -1,7 +1,7 @@
-import Axios from 'axios';
 import { Error } from 'components/Error';
 import { Loading } from 'components/Loading';
 import * as React from 'react';
+import { Fetch } from 'react-refetch-component';
 import { Col, Container, Row } from 'reactstrap';
 import { Button } from 'reactstrap';
 import { API_URL } from '../../constants';
@@ -9,10 +9,7 @@ import { API_URL } from '../../constants';
 const JewishCard = ({ image, title, link }) => {
   return (
     <div className="jewish-card center">
-      <img
-        src="https://s3.amazonaws.com/images.shoreshdavidbrandon.com/bg-image.svg"
-        alt={title}
-      />
+      <img src={image} alt={title} />
       <div className="jewish-card__body">
         <a href={link} target="_blank" rel="noopener noreferrer">
           <h3 className="jewish-card-title">{title}</h3>
@@ -27,55 +24,33 @@ const JewishCard = ({ image, title, link }) => {
   );
 };
 
-export class JewishPage extends React.Component<any, any> {
-  public state = {
-    loading: true,
-    error: null,
-    jewish: [] as any,
-  };
-  public componentDidMount() {
-    Axios.get(`${API_URL}/jewish`)
-      .then(res => {
-        this.setState({ loading: false, jewish: res.data.data });
-      })
-      .catch(err => {
-        this.setState({ loading: false, error: err });
-      });
-  }
-  public render() {
-    if (this.state.loading) {
-      return <Loading />;
-    }
-    if (this.state.error) {
-      return <Error error={this.state.error} />;
-    }
-    if (!this.state.jewish.length) {
-      return (
-        <Container fluid className="padding-50">
-          <Row>
-            <Col lg="3">
-              <h1>No Jewish items yet.</h1>
-            </Col>
-          </Row>
-        </Container>
-      );
-    }
-    return (
-      <Container fluid className="padding-50">
-        <div className="jewish-page">
-          <Row>
-            {this.state.jewish.map(j => (
-              <Col sm="12" lg="3" key={j._id}>
-                <JewishCard
-                  image={j.featuredImage}
-                  title={j.title}
-                  link={j.link}
-                />
-              </Col>
-            ))}
-          </Row>
-        </div>
-      </Container>
-    );
-  }
-}
+export const JewishPage = () => {
+  return (
+    <Container fluid className="padding-50">
+      <div className="jewish-page">
+        <Row>
+          <Fetch url={`${API_URL}/jewish`} method="get" lifecycle="onMount">
+            {({ loading, error, data }) => {
+              if (loading) {
+                return <Loading />;
+              }
+              if (error) {
+                return <Error error={error} />;
+              }
+
+              return data.data.map(j => (
+                <Col sm="12" lg="3" key={j._id}>
+                  <JewishCard
+                    image={j.featuredImage}
+                    title={j.title}
+                    link={j.link}
+                  />
+                </Col>
+              ));
+            }}
+          </Fetch>
+        </Row>
+      </div>
+    </Container>
+  );
+};
--- src/pages/ProductListPage/index.tsx
+++ src/pages/ProductListPage/index.tsx
@@ -1,61 +1,36 @@
-import Axios from 'axios';
 import { Error } from 'components/Error';
 import { Loading } from 'components/Loading';
 import { Product } from 'components/Product';
 import * as React from 'react';
+import { Fetch } from 'react-refetch-component';
 import { Col, Container, Row } from 'reactstrap';
 import { API_URL } from '../../constants';
 
-export class ProductListPage extends React.Component<any, any> {
-  public state = {
-    loading: true,
-    error: null,
-    products: [] as any,
-  };
+export const ProductListPage = () => {
+  return (
+    <Container fluid className="padding-50">
+      <Row>
+        <Fetch url={`${API_URL}/products`} method="get" lifecycle="onMount">
+          {({ loading, error, data }) => {
+            if (loading) {
+              return <Loading />;
+            }
+            if (error) {
+              return <Error error={error} />;
+            }
 
-  public componentDidMount() {
-    Axios.get(`${API_URL}/products`)
-      .then(res => {
-        this.setState({
-          loading: false,
-          products: res.data.data,
-        });
-      })
-      .catch(err => {
-        this.setState({
-          loading: false,
-          error: err,
-        });
-      });
-  }
-  public render() {
-    if (this.state.loading) {
-      return <Loading />;
-    }
-    if (this.state.error) {
-      return <Error error={this.state.error} />;
-    }
-    if (!this.state.products.length) {
-      return (
-        <Container fluid className="padding-50">
-          <Row>
-            <Col lg="3">
-              <h1>No Products yet</h1>
-            </Col>
-          </Row>
-        </Container>
-      );
-    }
-    return (
-      <Container fluid className="padding-50">
-        <Row>
-          {this.state.products.map(product => (
-            <Col lg="3" key={product.slug}>
-              <Product product={product} />
-            </Col>
-          ))};
-        </Row>
-      </Container>
-    );
-  }
-}
+            if (!data.data.length) {
+              return <div>No products yet.</div>;
+            }
+
+            return data.data.map(product => (
+              <Col lg="3" key={product.slug}>
+                <Product product={product} />
+              </Col>
+            ));
+          }}
+        </Fetch>
+      </Row>
+    </Container>
+  );
+};
