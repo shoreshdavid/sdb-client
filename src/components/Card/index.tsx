80b5b12a7f73f3d38f7025e2b12bf565dc411e55
Clayton Ray <iamclaytonray@gmail.com>
2018-12-07 03:50:47 -0500

    style: update card ui, use transparent bg image and dynamic background color

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import { Image } from '../Image';
+
+import './card.scss';
+
+// purple 5A17C7
+// royal blue 031AF7
+// light blue 00ADFF
+
+const colors = ['#5A17C7', '#031AF7', '#08D316', '#00ADFF', '#FF4600', 'red'];
 
 export const Card = ({
   featuredImage,
@@ -9,30 +16,34 @@ export const Card = ({
   isSeries,
   link,
   type,
-}: any) => (
-  <div className="card-item">
-    <div className="card-thumb">
-      <Image
-        src={featuredImage}
-        className="lazyload attachment-large size-large"
-        alt={slug}
-      />
-      {isSeries ? (
-        <Link to={`/${type}/${slug}`}>
-          <button className="btn btn-primary btn-sm card-series">Series</button>
-        </Link>
-      ) : null}
+}: any) => {
+  const rand = colors[Math.floor(Math.random() * colors.length)];
+
+  return (
+    <div className="card-item">
+      <div className="card-thumb" style={{ backgroundColor: rand }}>
+        <div className="card-thumb-title">
+          <span>{title}</span>
+        </div>
+        {isSeries ? (
+          <Link to={`/${type}/${slug}`}>
+            <button className="btn btn-primary btn-sm card-series">
+              Series
+            </button>
+          </Link>
+        ) : null}
+      </div>
+      <div className="card-content">
+        <h4 className="card-title">
+          {link ? (
+            <a href={link} target="_blank">
+              {title}
+            </a>
+          ) : (
+            <Link to={`/${type}/${slug}`}>{title}</Link>
+          )}
+        </h4>
+      </div>
     </div>
-    <div className="card-content">
-      <h4 className="card-title">
-        {link ? (
-          <a href={link} target="_blank">
-            {title}
-          </a>
-        ) : (
-          <Link to={`/${type}/${slug}`}>{title}</Link>
-        )}
-      </h4>
-    </div>
-  </div>
-);
+  );
+};
