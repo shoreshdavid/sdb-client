83c17ee2418d072a1e3661021042488c6ec93c9e
Clayton Ray <iamclaytonray@gmail.com>
2018-12-15 11:21:17 -0500

    style: update footer styling

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -17,59 +17,60 @@ export const Footer = () => {
   return (
     <footer>
       <div className="footer-copyright">
-        2018 © Shoresh David of Brandon | All Rights Reserved |{' '}
-        <Link to="/privacy-policy"> Privacy Policy</Link>
-      </div>
-
-      <div className="footer-social">
-        {/* Facebook */}
-        <a
-          href="https://www.facebook.com/shoreshdavidbrandon/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faFacebook} />
-        </a>
-        {/* Twitter */}
-        <a
-          href="https://twitter.com/shoreshdavid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faTwitter} />
-        </a>
-        {/* Twitter */}
-        <a
-          href="https://instagram.com/shoreshdavidbrandon"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faInstagram} />
-        </a>
-        {/* YouTube */}
-        <a
-          href="https://www.youtube.com/channel/UC1yOFqmKt-1ruHxg6fBiAFg?view_as=subscriber"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faYoutube} />
-        </a>
-        {/* Medium */}
-        <a
-          href="https://medium.com/@shoreshdavidbrandon"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faMedium} />
-        </a>
-        {/* Anchor */}
-        <a
-          href="https://anchor.fm/shoresh-david-brandon"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FontAwesomeIcon icon={faAnchor} />
-        </a>
+        <span>
+          2018 © Shoresh David of Brandon | All Rights Reserved |{' '}
+          <Link to="/privacy-policy"> Privacy Policy</Link>
+        </span>
+        <div className="footer-social">
+          {/* Facebook */}
+          <a
+            href="https://www.facebook.com/shoreshdavidbrandon/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faFacebook} />
+          </a>
+          {/* Twitter */}
+          <a
+            href="https://twitter.com/shoreshdavid"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+          {/* Twitter */}
+          <a
+            href="https://instagram.com/shoreshdavidbrandon"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+          {/* YouTube */}
+          <a
+            href="https://www.youtube.com/channel/UC1yOFqmKt-1ruHxg6fBiAFg?view_as=subscriber"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faYoutube} />
+          </a>
+          {/* Medium */}
+          <a
+            href="https://medium.com/@shoreshdavidbrandon"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faMedium} />
+          </a>
+          {/* Anchor */}
+          <a
+            href="https://anchor.fm/shoresh-david-brandon"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faAnchor} />
+          </a>
+        </div>
       </div>
     </footer>
   );
