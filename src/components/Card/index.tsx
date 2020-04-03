39a7bc912ee8335fe24d620a623c7fad41c9f984
Clayton Ray <iamclaytonray@gmail.com>
2020-04-02 23:48:06 -0400

    feat: refactor card ui

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,59 +1,44 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 
+import jewishImg from '../../assets/img/jewish.png';
+import serviceImg from '../../assets/img/service-background-new.png';
+
 import './card.scss';
 
-const jewishImg = require('../../assets/img/jewish.png');
-const serviceImg = require('../../assets/img/service-background-new.png');
-
-export const Card = ({
-  featuredImage,
-  title,
-  slug,
-  isSeries,
-  link,
-  color,
-  type,
-  category,
-}: any) => {
-  const img =
-    type === 'jewish' ? `url("${jewishImg}")` : `url("${serviceImg}")`;
-
-  const CustomLink = ({children}) => {
-    if (link) {
-      return (
-        <a href={link} target="_blank">
-          {children}
-        </a>
-      );
-    }
-    if (type === 'jewish') {
-      return <Link to={`/${type}/${slug}`}>{children}</Link>
-    }
-    
-    return <Link to={`/${type}/${category}/${slug}`}>{children}</Link>
-    
+const CustomLink = ({ link, type, category, slug, children }) => {
+  if (link) {
+    return (
+      <a href={link} target="_blank">
+        {children}
+      </a>
+    );
+  }
+  if (type === 'jewish') {
+    return <Link to={`/${type}/${slug}`}>{children}</Link>;
   }
 
+  return <Link to={`/${type}/${category}/${slug}`}>{children}</Link>;
+};
 
+export const Card = ({ title, slug, link, color, type, category }) => {
+  const img =
+    type === 'jewish' ? `url("${jewishImg}")` : `url("${serviceImg}")`;
+  console.log(img);
   return (
-    <CustomLink>
-        <div className="card-item">
-          <div
-            className="card-thumb"
-            style={{
-              backgroundImage: img,
-              backgroundColor: color,
-            }}
-          >
-            <div className="card-thumb-title">
-              <span>{title}</span>
-            </div>
-          </div>
-          <div className="card-content">
-            <h4 className="card-title">{title}</h4>
-          </div>
+    <div className="sdb-card">
+      <CustomLink type={type} link={link} slug={slug} category={category}>
+        <div
+          className="sdb-card_img"
+          style={{
+            backgroundImage: img,
+            backgroundColor: color,
+          }}
+        />
+        <div className="sdb-card-body">
+          <h4>{title}</h4>
         </div>
       </CustomLink>
+    </div>
   );
 };
