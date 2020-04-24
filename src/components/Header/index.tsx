da9ff731cd023ff2329ca16160ac2b50ad9c1533
Clayton Ray <iamclaytonray@gmail.com>
2020-04-24 12:46:52 -0400

    refactor: make header functional & make donate link a button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@material-ui/core';
 import * as React from 'react';
-import { Link, NavLink, withRouter } from 'react-router-dom';
+import { Link, NavLink, useHistory, useLocation } from 'react-router-dom';
 import { persistor } from '~store/store';
 
 import headerLogo from '../../assets/img/header-logo.png';
@@ -9,182 +9,169 @@ import { Image } from '../../components/Image';
 
 import './header.scss';
 
-export class WrappedHeader extends React.Component<any, any> {
-  public state = {
-    isOpen: false,
-  };
+export const Header = () => {
+  const history = useHistory();
+  const location = useLocation();
+  const [isOpen, setIsOpen] = React.useState(false);
 
-  public componentDidMount() {
+  React.useEffect(() => {
     document.addEventListener('click', (event: any) => {
       if (event.target.tagName.toLowerCase() === 'a') {
-        this.toggle();
+        toggle();
       }
     });
-  }
+    return () =>
+      document.addEventListener('click', () => {
+        this.toggle();
+      });
+  },              []);
 
-  public toggle = () => {
-    this.setState({
-      isOpen: !this.state.isOpen,
-    });
-  }
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
 
-  public handleDeleteCache = () => {
+  const handleDeleteCache = () => {
     persistor.purge();
-  }
+  };
 
-  public componentWillUnmount() {
-    document.addEventListener('click', () => {
-      this.toggle();
-    });
-  }
+  return (
+    <div className="sdb-nav">
+      <nav className="navbar navbar-expand-xl navbar-light">
+        <Link to="/" style={{ marginLeft: 0, flex: 1 }}>
+          {window.innerWidth < 600 ? (
+            <Image
+              src={smallLogo}
+              style={{ width: 50, height: 50 }}
+              alt="Shoresh David Brandon logo"
+            />
+          ) : (
+            <Image src={headerLogo} alt="Shoresh David Brandon logo" />
+          )}
+        </Link>
 
-  public render() {
-    return (
-      <div className="sdb-nav">
-        <nav className="navbar navbar-expand-xl navbar-light">
-          <Link to="/" style={{ marginLeft: 0, flex: 1 }}>
-            {window.innerWidth < 600 ? (
-              <Image
-                src={smallLogo}
-                style={{ width: 50, height: 50 }}
-                alt="Shoresh David Brandon logo"
-              />
-            ) : (
-              <Image src={headerLogo} alt="Shoresh David Brandon logo" />
-            )}
-          </Link>
-
-          <button
-            type="button"
-            className="navbar-toggler"
-            onClick={this.toggle}
-          >
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div
-            className={`${
-              this.state.isOpen
-                ? 'collapse show navbar-collapse'
-                : 'collapse navbar-collapse'
-            }`}
-          >
-            <ul className="ml-auto navbar-nav" style={{ marginRight: 25 }}>
-              <li className="nav-item">
-                <NavLink exact to="/" activeClassName="navlink-active">
-                  Home
+        <button type="button" className="navbar-toggler" onClick={toggle}>
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div
+          className={`${
+            isOpen
+              ? 'collapse show navbar-collapse'
+              : 'collapse navbar-collapse'
+          }`}
+        >
+          <ul className="ml-auto navbar-nav" style={{ marginRight: 25 }}>
+            <li className="nav-item">
+              <NavLink exact to="/" activeClassName="navlink-active">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item dropdown-link">
+              <div className="dropdown">
+                <NavLink
+                  to="/services/rabbi-don"
+                  isActive={() => location.pathname.includes('services')}
+                  activeClassName="navlink-active"
+                >
+                  Teachings
                 </NavLink>
-              </li>
-              <li className="nav-item dropdown-link">
-                <div className="dropdown">
-                  <NavLink
-                    to="/services/rabbi-don"
-                    isActive={() =>
-                      this.props.location.pathname.includes('services')
-                    }
-                    activeClassName="navlink-active"
+                <div className="dropdown-content">
+                  <Link to="/services/rabbi-don">
+                    Rabbi Don's Teachings &amp; Others
+                  </Link>
+                  <Link to="/services/get-healed-fast/how-to-get-healed-and-stay-healed">
+                    Get Healed Fast
+                  </Link>
+                  <Link to="/services/holy-days">"Jewish" Holidays</Link>
+                  <Link to="/services/end-times">End Times</Link>
+                  <Link to="/services/bible-studies">Bible Studies</Link>
+                  <Link to="/services/special-speakers">Special Speakers</Link>
+                  <Link to="/services/increase-your-income">
+                    Increase Your Income
+                  </Link>
+                  <a
+                    href="https://www.ihaveafriendwhoisjewish.com/"
+                    target="_blank"
                   >
-                    Teachings
-                  </NavLink>
-                  <div className="dropdown-content">
-                    <Link to="/services/rabbi-don">
-                      Rabbi Don's Teachings &amp; Others
-                    </Link>
-                    <Link to="/services/get-healed-fast/how-to-get-healed-and-stay-healed">
-                      Get Healed Fast
-                    </Link>
-                    <Link to="/services/holy-days">"Jewish" Holidays</Link>
-                    <Link to="/services/end-times">End Times</Link>
-                    <Link to="/services/bible-studies">Bible Studies</Link>
-                    <Link to="/services/special-speakers">
-                      Special Speakers
-                    </Link>
-                    <Link to="/services/increase-your-income">
-                      Increase Your Income
-                    </Link>
-                    <a
-                      href="https://www.ihaveafriendwhoisjewish.com/"
-                      target="_blank"
-                    >
-                      Rabbi Don's Best Selling Book
-                    </a>
-                    <a
-                      // tslint:disable-next-line
-                      href="https://s3.amazonaws.com/images.shoreshdavidbrandon.com/pdf/discipleship-book-by-rabbi-don-goldstein.pdf"
-                      target="_blank"
-                    >
-                      Rabbi Don's Discipleship Book
-                    </a>
-                  </div>
-                </div>
-              </li>
-
-              <li className="nav-item">
-                <NavLink exact to="/books" activeClassName="navlink-active">
-                  Books
-                </NavLink>
-              </li>
-              {/*  */}
-              <li className="nav-item">
-                <NavLink exact to="/live" activeClassName="navlink-active">
-                  Streaming
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink exact to="/jewish" activeClassName="navlink-active">
-                  Jewish
-                </NavLink>
-              </li>
-              {/*  */}
-              <li className="nav-item dropdown-link">
-                <div className="dropdown">
-                  <NavLink
-                    to="/discoveries/evolution"
-                    isActive={() =>
-                      this.props.location.pathname.includes('discoveries')
-                    }
-                    activeClassName="navlink-active"
+                    Rabbi Don's Best Selling Book
+                  </a>
+                  <a
+                    // tslint:disable-next-line
+                    href="https://s3.amazonaws.com/images.shoreshdavidbrandon.com/pdf/discipleship-book-by-rabbi-don-goldstein.pdf"
+                    target="_blank"
                   >
-                    Discoveries
-                  </NavLink>
-                  <div className="dropdown-content">
-                    <Link to="/discoveries/evolution">Evolution</Link>
-                    <Link to="/discoveries/heaven-and-hell">
-                      Heaven And Hell
-                    </Link>
-                    <Link to="/discoveries/prophecy">Prophecy</Link>
-                    <Link to="/discoveries/supernatural">Supernatural</Link>
-                    <Link to="/discoveries/usa-middle-east">
-                      USA &amp; Middle East
-                    </Link>
-                    <Link to="/discoveries/life-issues">Life Issues</Link>
-                  </div>
+                    Rabbi Don's Discipleship Book
+                  </a>
                 </div>
-              </li>
-              {/*  */}
-              <li className="nav-item">
-                <NavLink exact to="/events" activeClassName="navlink-active">
-                  Events
+              </div>
+            </li>
+
+            <li className="nav-item">
+              <NavLink exact to="/books" activeClassName="navlink-active">
+                Books
+              </NavLink>
+            </li>
+            {/*  */}
+            <li className="nav-item">
+              <NavLink exact to="/live" activeClassName="navlink-active">
+                Streaming
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink exact to="/jewish" activeClassName="navlink-active">
+                Jewish
+              </NavLink>
+            </li>
+            {/*  */}
+            <li className="nav-item dropdown-link">
+              <div className="dropdown">
+                <NavLink
+                  to="/discoveries/evolution"
+                  isActive={() => location.pathname.includes('discoveries')}
+                  activeClassName="navlink-active"
+                >
+                  Discoveries
                 </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink exact to="/store" activeClassName="navlink-active">
-                  Store
-                </NavLink>
-              </li>
-              <li className="nav-item">
+                <div className="dropdown-content">
+                  <Link to="/discoveries/evolution">Evolution</Link>
+                  <Link to="/discoveries/heaven-and-hell">Heaven And Hell</Link>
+                  <Link to="/discoveries/prophecy">Prophecy</Link>
+                  <Link to="/discoveries/supernatural">Supernatural</Link>
+                  <Link to="/discoveries/usa-middle-east">
+                    USA &amp; Middle East
+                  </Link>
+                  <Link to="/discoveries/life-issues">Life Issues</Link>
+                </div>
+              </div>
+            </li>
+            {/*  */}
+            <li className="nav-item">
+              <NavLink exact to="/events" activeClassName="navlink-active">
+                Events
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink exact to="/store" activeClassName="navlink-active">
+                Store
+              </NavLink>
+            </li>
+            {/* <li className="nav-item">
                 <NavLink exact to="/donate" activeClassName="navlink-active">
                   Donate
                 </NavLink>
-              </li>
-            </ul>
-            <Button color="secondary" onClick={this.handleDeleteCache}>
-              Delete Cache
-            </Button>
-          </div>
-        </nav>
-      </div>
-    );
-  }
-}
-
-export const Header = withRouter(WrappedHeader);
+              </li> */}
+          </ul>
+          <Button
+            color="primary"
+            style={{ marginRight: 8 }}
+            onClick={() => history.push('/donate')}
+          >
+            Donate
+          </Button>
+          <Button color="secondary" onClick={handleDeleteCache}>
+            Delete Cache
+          </Button>
+        </div>
+      </nav>
+    </div>
+  );
+};
