4f3a87a04e15c1e3e34c38db6844ff7e74f689a1
Clayton Ray <iamclaytonray@gmail.com>
2020-04-24 10:07:03 -0400

    fix: use correct api url based on env

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,12 @@
-// export const LOCAL_API = 'http://localhost:3000/v2';
+export const LOCAL_API = 'http://localhost:3000/v2';
 export const STAGING_API = 'https://staging-sdb-api.herokuapp.com/v2';
-// export const PROD_API = 'https://api.shoreshdavidbrandon.org/v1';
+export const PROD_API = 'https://api.shoreshdavidbrandon.org/v1';
 
 const nodeEnv = process.env.NODE_ENV;
 
 export const API_URL =
   nodeEnv === 'production'
-    ? STAGING_API
+    ? PROD_API
     : nodeEnv === 'staging'
     ? STAGING_API
-    : STAGING_API;
+    : LOCAL_API;
