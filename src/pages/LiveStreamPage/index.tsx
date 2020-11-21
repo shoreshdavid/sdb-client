064ad6a709c785f8a43f345266aa684db1005041
Shoresh David Brandon <43940697+shoreshdavidbrandon@users.noreply.github.com>
2020-11-20 19:15:32 -0500

    Update index.tsx

--- src/pages/LiveStreamPage/index.tsx
+++ src/pages/LiveStreamPage/index.tsx
@@ -27,7 +27,7 @@ export const LiveStreamPage = () => (
       <div className="row">
         <div className="col-lg-8">
           <iframe
-            src="https://www.youtube.com/embed/-PtkwYvrTwA"
+            src="https://www.youtube.com/embed/9Ggsq9hDKg8"
             frameBorder="0"
             scrolling="no"
             allowFullScreen
