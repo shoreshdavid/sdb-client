90ad390cb085d917a21882127806aa371871b348
Clayton Ray <iamclaytonray@gmail.com>
2021-01-29 19:17:14 -0500

    Update index.tsx

--- src/pages/LiveStreamPage/index.tsx
+++ src/pages/LiveStreamPage/index.tsx
@@ -27,7 +27,7 @@ export const LiveStreamPage = () => (
       <div className="row">
         <div className="col-lg-8">
           <iframe
-            src="https://www.youtube.com/embed/fzlxoeRfGqA"
+            src="https://www.youtube.com/embed/pDTVwz1C0NU"
             frameBorder="0"
             scrolling="no"
             allowFullScreen
